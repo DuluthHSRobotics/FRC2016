fb2296511b7aa158768770f23864fa4ca0d97622
Joshua Bowden <josbow@gmail.com>
2015-10-13 20:53:15 -0400

    Fix NPE with the new DrivingScalingPref. Still acting kinda strange.

--- src/org/usfirst/frc5293/prefs/DriveScalingPref.java
+++ src/org/usfirst/frc5293/prefs/DriveScalingPref.java
@@ -4,42 +4,46 @@ import org.usfirst.frc5293.prefs.util.BooleanPref;
 import org.usfirst.frc5293.prefs.util.DoublePref;
 import org.usfirst.frc5293.prefs.util.Pref;
 import org.usfirst.frc5293.prefs.util.PrefGroup;
-import org.usfirst.frc5293.translations.util.DrivingState;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class DriveScalingPref implements PrefGroup {
     private final List<Pref<?>> all = new ArrayList<>();
-    private String name;
+    private final String name;
+
+    private final BooleanPref isEnabled;
+    private final DoublePref x;
+    private final DoublePref y;
+    private final DoublePref rotation;
 
     public DriveScalingPref(String name) {
-        this.name = name;
-
-        if (this.name == null || this.name.isEmpty()) {
+        if (name == null || name.isEmpty()) {
             this.name = "default";
+        } else {
+            this.name = name;
         }
+
+        isEnabled = new BooleanPref(
+                "drivetrain:scaling:" + name + ":isEnabled",
+                true
+        ); { all.add(isEnabled); }
+
+        x = new DoublePref(
+                "drivetrain:scaling:" + name + ":x",
+                1.0
+        ); { all.add(x); }
+
+        y = new DoublePref(
+                "drivetrain:scaling:" + name + ":y",
+                1.0
+        ); { all.add(y); }
+
+        rotation = new DoublePref(
+                "drivetrain:scaling:" + name + ":rotation",
+                1.0
+        ); { all.add(rotation); }
     }
-
-    private final BooleanPref isEnabled = new BooleanPref(
-            "drivetrain:scaling:" + name + ":isEnabled",
-            true
-    ); { all.add(isEnabled); }
-
-    private final DoublePref x = new DoublePref(
-            "drivetrain:scaling:" + name + ":x",
-            1.0
-    ); { all.add(x); }
-
-    private final DoublePref y = new DoublePref(
-            "drivetrain:scaling:" + name + ":y",
-            1.0
-    ); { all.add(y); }
-
-    private final DoublePref rotation = new DoublePref(
-            "drivetrain:scaling:" + name + ":rotation",
-            1.0
-    ); { all.add(rotation); }
 
     @Override
     public List<Pref<?>> getAll() {
